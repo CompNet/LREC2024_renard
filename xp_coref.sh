750094125392e3e06caedf27b03e61fe497db931
Aethor <oow.autre@gmail.com>
2023-09-20 12:29:48 +0200

    xp_coref.sh: checkout litbank commit for reproducibility

--- xp_coref.sh
+++ xp_coref.sh
@@ -2,6 +2,10 @@
 
 if [[ ! -d './litbank' ]]; then
     git clone https://github.com/dbamman/litbank.git
+    # checkout a specific commit for reproducibility
+    pushd ./litbank
+    git checkout '3e50db0ffc033d7ccbb94f4d88f6b99210328ed8'
+    popd
 fi
 
 python xp_coref.py with litbank_path='./litbank'
